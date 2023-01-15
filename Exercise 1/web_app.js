f100b6226825cd301a23dd39ef6e2d66bbfd8787
Trym Gudvangen <trym.gudvangen@gmail.com>
2023-01-15 11:30:11 +0100

    feat: create basic webpage structure

---
+++ Exercise 1/web_app.js
@@ -0,0 +1,4 @@
+// Idea of the page: Where is waldo?
+// A button to randomize his location and one button to hide him, turning the screen black.
+// When the mouse hovers over a location, it acts like a spotlight.
+// I could also have time...
