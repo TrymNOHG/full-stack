ccbf823a97a46adf681770b05c506288466f3d5e
Trym Gudvangen <trym.gudvangen@gmail.com>
2023-03-03 15:43:07 +0100

    refactor: use ResponseEntity

--- exercise-4/backend/src/main/java/edu/ntnu/trym/exercise4/controller/CalculationController.java
+++ exercise-4/backend/src/main/java/edu/ntnu/trym/exercise4/controller/CalculationController.java
@@ -4,7 +4,7 @@ import edu.ntnu.trym.exercise4.model.Expression;
 import edu.ntnu.trym.exercise4.service.CalculationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,16 +22,15 @@ public class CalculationController {
     @PostMapping("/calculate")
     @CrossOrigin(origins="*")
     @ResponseBody
-    public double calculate(@RequestBody Expression expression) {
+    public ResponseEntity<Double> calculate(@RequestBody Expression expression) {
         try {
             double result = this.calculationService.calculate(expression);
             logger.info("The expression yielded: " + result);
-            return result;
+            return new ResponseEntity<>(result, HttpStatus.OK);
         } catch(IllegalArgumentException e) {
             logger.warn(e.getMessage());
-            throw e;
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
 
 }
-//TODO: look at ResponseEntity<T>
